4942b8287160fb39a095a0b3e4ffab4874d1ba3d
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-17 10:34:39 +0200

    wired up simple ReadStory page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { getUserWithStoredToken } from "./store/user/actions";
 import { Jumbotron } from "react-bootstrap";
 import StoryList from "./pages/StoryList";
 import LandingPage from './pages/LandingPage';
+import ReadStory from "./pages/ReadStory";
 
 const Other = () => (
   <Jumbotron>
@@ -37,7 +38,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/stories" component={StoryList} />
-        <Route path="/stories/:id" />
+        <Route path="/stories/:id" component={ReadStory} />
         <Route path="/other" component={Other} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
---
+++ src/pages/ReadStory/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ReadStory() {
+  return (
+    <div className='read-story'>
+      HERE YOU READ A STORY
+    </div>
+  )
+}
