386ba3b3d384ca10d48bea84866089f5ee9ab2a6
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-17 10:40:20 +0200

    created simple fetchStoryById thunk

--- src/pages/ReadStory/index.js
+++ src/pages/ReadStory/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchStoryById } from '../../store/readStory/actions';
 
 export default function ReadStory() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(fetchStoryById(id))  
+  },
+  [])
+
   return (
     <div className='read-story'>
       HERE YOU READ A STORY
---
+++ src/store/readStory/actions.js
@@ -0,0 +1,8 @@
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+
+export const fetchStoryById = id => {
+  return async (dispatch, getState) => {
+    console.log('I am working!!')
+  }
+}
