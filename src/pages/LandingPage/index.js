2bf677ba986394ff86fb44db9f55c23988b0bd0f
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-23 12:07:15 +0200

    fixed background img, created dynamic button

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import { NavLink } from "react-router-dom";
+import { selectUser } from '../../store/user/selectors';
 import Button from 'react-bootstrap/Button';
 import logo from "../../images/logo-white.png";
 import './landingpage.css';
 
+
 export default function LandingPage() {
+  const user = useSelector(selectUser);
+  const button = user.name === null ? 'Sluit je aan' : 'Welkom terug';
+  const to = user.name === null ? '/signup' : '/stories';
+
   return (
     <div className="landing-page">
       <div className="hero-image">
@@ -11,7 +19,7 @@ export default function LandingPage() {
         <div className="hero-text">
           <p>Voor en door...</p>
           <span className='schrijvers'>Schrijvers</span> <br/>
-          <Button variant="success">Sluit je aan</Button> <br/>
+          <Button variant="success" as={NavLink} to={to}>{button}</Button> <br/>
           <img alt="" src={logo} width="100" height="100" />
         </div>
       </div>
