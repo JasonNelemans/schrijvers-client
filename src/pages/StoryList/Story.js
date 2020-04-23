2c81298f637a202fa8dacfabf15409d24ab59c42
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-23 14:04:00 +0200

    storylist styling and translated to Dutch

--- src/pages/StoryList/Story.js
+++ src/pages/StoryList/Story.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import "./index.css";
 import { NavLink } from "react-router-dom";
 import { updateTitleClicked } from "../../store/readStory/actions";
@@ -10,19 +10,26 @@ export default function Story(props) {
   const dispatch = useDispatch();
 
   const clickHandler = () => {
-    dispatch(updateTitleClicked(props.id, props.titleClicked))
-  }
+    dispatch(updateTitleClicked(props.id, props.titleClicked));
+  };
 
   return (
     <div className="story-container">
-      <img width="70" height="90" alt="" />
+      <img width="70" height="90" alt=""/>
       <div className="story-text">
-        <NavLink to={`/stories/${props.id}`} exact={true} onClick={clickHandler}>
-          <h3>{props.title}</h3>
+        <NavLink
+          to={`/stories/${props.id}`}
+          exact={true}
+          onClick={clickHandler}
+        >
+          <h3 style={{ fontFamily: "sans serif", fontSize: "30px", margin: 0 }}>
+            {props.title}{" "}
+          </h3>
         </NavLink>
+        <span className="author"> door: {props.user.name}</span> <br/>
         <p>
           <span>{props.pg}</span> <strong>Type: </strong> {props.type} (
-          <em>{props.genre}</em>) Word count: {props.wordCount}
+          <em>{props.genre}</em>) Aantal woorden: {props.wordCount}
         </p>
         <span className="posted">Posted: {time} </span>
       </div>
